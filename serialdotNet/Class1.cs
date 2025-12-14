ccba6a83002d60443603e9706231b617a9d85c84
Micha <159259755+Roehrenzauber@users.noreply.github.com>
2025-12-14 10:30:43 +0100

    s

---
+++ serialdotNet/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace serialdotNet
+{
+    internal class Class1
+    {
+    }
+}
---
+++ serialdotNet/Form1.cs
@@ -0,0 +1,10 @@
+namespace serialdotNet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
